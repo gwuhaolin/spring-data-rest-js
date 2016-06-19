7e9781defb655163e3ee385b328902c95f786a91
WuHaolin <gwuhaolin@gmail.com>
2016-06-19 17:01:38 +0800

    fix for npm webpack use

--- browser.ts
+++ browser.ts
@@ -1,2 +1,3 @@
 import * as spring from './spring'
 window['spring'] = spring;
+export default spring;
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="typings/index.d.ts" />
 declare module "node-fetch" {
     export default function fetch(url:string|Request, init?:RequestInit):Promise<Response>;
 }
